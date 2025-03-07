bc95d010e142eb2334282c5e989a77af741a847a
lucasolopes <olopes.lucas567@gmail.com>
2025-03-07 01:21:24 -0300

    Reorganiza estrutura do projeto e integra comandos ao MediatR
    Removido grupo de pastas do arquivo `Application.csproj` para reorganização. Adicionadas diretivas `using MediatR;` e namespaces nos arquivos de comando (`SyncDistrictsCommand.cs`, `SyncMesoregionsCommand.cs`, `SyncMicroregionsCommand.cs`, `SyncMunicipalitiesCommand.cs`, `SyncStatesCommand.cs`, `SyncSubDistrictsCommand.cs`). Os comandos agora implementam a interface `IRequest`, facilitando a integração com o padrão de mediador.

---
+++ Core/Application/Commands/IbgeSync/SyncMesoregionsCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Application.Commands.IbgeSync;
+
+public record SyncMesoregionsCommand() : IRequest;
---
+++ Core/Application/Commands/IbgeSync/SyncSubDistrictsCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Application.Commands.IbgeSync;
+
+public record SyncSubDistrictsCommand() : IRequest;
