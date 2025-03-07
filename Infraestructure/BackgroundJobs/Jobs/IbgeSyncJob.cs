92fe7faceffc8dfe4cfdd6129b8cace8d879969b
lucasolopes <olopes.lucas567@gmail.com>
2025-03-06 23:32:40 -0300

    Adiciona suporte ao IBGE e atualiza dependências
    Implementa a classe `AssemblyReference` para referência de assembly. Atualiza o projeto para .NET 8.0 com usings implícitos e nullable. Adiciona pacotes do Quartz. Cria a classe `IbgeSyncJob` para sincronização de dados do IBGE utilizando MediatR, com tratamento de exceções e logs.

---
+++ Infraestructure/BackgroundJobs/Jobs/IbgeSyncJob.cs
@@ -0,0 +1,54 @@
+﻿
+using Application.Commands.IbgeSync;
+using Domain.Entities;
+using MediatR;
+using Quartz;
+
+namespace BackgroundJobs.Jobs;
+
+[DisallowConcurrentExecution]
+public class IbgeSyncJob : IJob
+{
+    private readonly IMediator _mediator;
+
+    public IbgeSyncJob(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine("Starting IBGE synchronization...");
+            
+        try
+        {
+            // Sincroniza regiões
+            await _mediator.Send(new SyncRegionsCommand());
+               /* 
+            // Sincroniza estados
+            await _mediator.Send(new SyncStatesCommand());
+                
+            // Sincroniza mesorregiões
+            await _mediator.Send(new SyncMesoregionsCommand());
+                
+            // Sincroniza microrregiões
+            await _mediator.Send(new SyncMicroregionsCommand());
+                
+            // Sincroniza municípios
+            await _mediator.Send(new SyncMunicipalitiesCommand());
+                
+            // Sincroniza distritos
+            await _mediator.Send(new SyncDistrictsCommand());
+                
+            // Sincroniza subdistritos
+            await _mediator.Send(new SyncSubDistrictsCommand());
+                */
+            Console.WriteLine("IBGE synchronization completed successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error during IBGE synchronization: {ex.Message}");
+            throw;
+        }
+    }
+}
